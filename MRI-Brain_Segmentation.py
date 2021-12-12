2158247def61a15265dd9c98853d8395923c97be
morteza <m67.heidari@gmail.com>
2021-12-12 15:46:58 -0500

    Bug Fixed.

--- MRI-Brain_Segmentation.py
+++ MRI-Brain_Segmentation.py
@@ -33,8 +33,8 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 ######
-# DataPath = "/kaggle/input/lgg-mri-segmentation/kaggle_3m/"  # Path to the data
-DataPath = r'C:\Users\Morteza.Heidari\OneDrive - BioTelemetry, Inc\github-projects\segmentation-and-classification\segmentation-and-classification-\datasets\LGG Segmentation Dataset\kaggle_3m'
+## Setting parametters
+DataPath = "/kaggle/input/lgg-mri-segmentation/kaggle_3m/"  # Path to the data
 EPOCHS = 35
 BATCH_SIZE = 32
 ImgHieght = 256
