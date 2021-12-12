470047bb66f4dfb3fa4421e789f3ba5deed34450
morteza <m67.heidari@gmail.com>
2021-12-12 18:52:47 -0500

    Libraries updated.

--- MRI-Brain_Segmentation.py
+++ MRI-Brain_Segmentation.py
@@ -14,16 +14,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
-from tqdm import tqdm_notebook, tnrange
-from glob import glob
-from skimage.io import imread, imshow, concatenate_images
-from skimage.transform import resize
-from skimage.morphology import label
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model, load_model, save_model
 import tensorflow as tf
-from skimage.color import rgb2gray
 from tensorflow.keras import Input
 from tensorflow.keras.layers import Input, Activation, BatchNormalization, Dropout, Lambda, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate
 from tensorflow.keras.optimizers import Adam
